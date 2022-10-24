e2a35c1434cfbc0c4517c4230312bb9c144b1bb2
stevejjk109 <stevejjk109@naver.com>
2022-10-25 01:43:47 +0900

    로그인화면 리덕스 적용

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { userslice } from "../Login/index/userslice";
+import { loginslice } from "../../components/Login/state/slice";
 
 const reducers = combineReducers({
   user: userslice.reducer,
+  login: loginslice.reducer,
 });
 
 export default reducers;
