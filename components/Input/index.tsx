0970dfca6c805f2de0522eabefcd1ec777826be0
hafnium1923 <hafnium1923@gmail.com>
2022-11-30 21:18:40 +0900

    feat input component

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,12 +1,26 @@
 import { Input } from 'antd';
 import React from 'react';
 
-const StyledInput = () => {
+const StyledInput = ({
+    placeholder,
+    style,
+    disabled = false,
+    ...restProps
+  }: {
+    placeholder: any;
+    style: any;
+    disabled: boolean;
+  }) => {
     return (
-        <>
-        <Input placeholder="Basic usageasd" />
-        </>
-    )
-}
+      <>
+        <Input
+          {...restProps}
+          placeholder={placeholder}
+          style={style}
+          disabled={disabled}
+        />
+      </>
+    );
+  };
 
 export default StyledInput;
